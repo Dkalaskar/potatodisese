d0fb730fd1217a556178ddb7601b33a50f451894
Dhananjay Kalaskar <kalaskard123@gmail.com>
2023-05-04 10:23:54 +0530

    Update main file

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 from io import BytesIO
@@ -6,6 +7,20 @@ from PIL import Image
 import tensorflow as tf
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 MODEL = tf.keras.models.load_model("D:/PotatoDisease/models/1")
 CLASS_NAMES = ["Early Blight","Late Blight","Healthy"]
 
